c7cc345b0025b11d55a860d9e12ee971724dde14
EricBonham <ericbonham@outlook.it>
2018-04-06 19:29:11 +0200

    Removed unnecessary print functions

--- Drop-Playlist for Reddit.py
+++ Drop-Playlist for Reddit.py
@@ -25,9 +25,7 @@ comments = post.comments
 comments.replace_more(int(config['Reddit']['limit']))
 Playlist=[]
 
-# regex 2.0 ((?:[A-Z](?:\w|\.)*\s)+(?:\s*-\s*)+(?:[A-Z]\w*\s)*(?:[A-Z](?:\w|\.)*)+)
-# regex 1.0 ((?:[A-Z][a-z]*\s)+(?:\s*-\s*)+(?:[A-Z][a-z]*\s)*(?:[A-Z][a-z]*)+)
-# best regex ever ((?:[A-Z][\w|\.|$|']*\s)+(?:\s*[-|by]\s*)+(?:[A-Z][\w|$|-|']*\s)*(?:[A-Z][\w|-|'|?]*)+)
+
  
 def SearchSong(text):
     regex = r"((?:[A-Z][\w|\.|$|']*\s)+(?:\s*[-|by]\s*)+(?:[A-Z][\w|$|-|']*\s)*(?:[A-Z][\w|-|'|?]*)+)"
@@ -41,19 +39,16 @@ for comment in comments:
     text = comment.body.replace('\n', ' ')
     SearchSong(text)
 
-print(len(Playlist))
-print(len(list(set(Playlist))))
+
 Playlist=list(set(Playlist))
 
 username = config['Spotify']['user_id']
 privacy = config['Spotify']['playlistprivacy']
-print(privacy)
 scope = 'playlist-modify-{}'.format(privacy)
 
 
 try:
     token = util.prompt_for_user_token(username, scope)
-    print(token)
 except:
     os.remove(".cache-{}".format(username))
     token = util.prompt_for_user_token(username)
@@ -103,13 +98,11 @@ IOLoop.current().run_sync(put_in_queue)
     
 
 #get synchronously songs that generated api errors
-print(LeftSongs)
-print(len(LeftSongs))
 for song in LeftSongs:
     results = spotify.search(q=LeftSongs[LeftSongs.index(song)], type='track', limit = 1)
     for i, t in enumerate(results['tracks']['items']):
         track_uris.append(t['uri'].replace('spotify:track:',''))
-print(len(track_uris))
+
 
 if privacy == 'private':
     playlists = spotify.user_playlist_create(username, config['Spotify']['playlistname'], public=False)
